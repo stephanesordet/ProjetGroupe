673a28fb3ba9cf27457de0385b334084d04341ad
stephane.sordet <stephane.sordet@SC-C232-PC11>
2019-09-12 14:01:14 +0200

    Ajout MLD

--- Gestionnaire de stock version 1.0/Gestionnaire de stock version 1.0/Form1.Designer.cs
+++ Gestionnaire de stock version 1.0/Gestionnaire de stock version 1.0/Form1.Designer.cs
@@ -58,7 +58,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(4, 13);
+            this.label1.Location = new System.Drawing.Point(4, 24);
             this.label1.Name = "label1";
             this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.label1.Size = new System.Drawing.Size(148, 13);
